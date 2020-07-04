257072b675fbb386c3eeff1b952090ba4cb2feaf
ﾑズﾑﾘ <66409915+Lazybotk@users.noreply.github.com>
2020-07-04 11:45:58 +0530

    Update fun.py

--- SaraRobot/modules/fun.py
+++ SaraRobot/modules/fun.py
@@ -592,6 +592,7 @@ __help__ = """
  • `/stickerid`*:* reply to a sticker to me to tell you its file ID.
 • `/getsticker`*:* reply to a sticker to me to upload its raw PNG file.
 • `/kang`*:* reply to a sticker to add it to your pack.
+• /lyrics <song> returns the lyrics of that song.
 """
 
 """
